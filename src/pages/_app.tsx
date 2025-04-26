c739d665f08077b25e9e1bd26af7bbb29f547e80
mkojima <kojima.masahiro.570@gmail.com>
2025-04-26 23:09:27 +0900

    Add Google Analytics integration to the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { AppProps } from 'next/app';
+import Script from 'next/script';
+import '../styles/globals.css'; // これは自分のスタイルパスに合わせて！
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-1K5LX0RGJ7"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1K5LX0RGJ7');
+          `,
+        }}
+      />
+      {/* アプリ本体 */}
+      <Component {...pageProps} />
+    </>
+  );
 }
