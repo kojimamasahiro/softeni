78dc2086e3b2e5974f874ca296fcb3b523e5313f
mkojima <kojima.masahiro.570@gmail.com>
2025-04-27 20:53:33 +0900

    Add favicon images for enhanced site branding

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ class MyDocument extends Document {
       <Html lang="ja">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
           <meta name="description" content="ソフトテニス選手の最新情報や試合結果をお届けするサイト" />
           <meta name="keywords" content="ソフトテニス, 試合結果, 大会, 選手" />
           <meta property="og:title" content="ソフトテニス選手情報サイト" />
