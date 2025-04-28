09c62b3d70aba32a82872deb646537c3f26ed0ce
mkojima <kojima.masahiro.570@gmail.com>
2025-04-28 10:58:39 +0900

    Recreate sitemap generation script and update sitemap.xml for player URLs

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+// プレイヤー一覧
+const players = ['uematsu-toshiki']; // ← 今後ここに増やせばOK！
+
+// サイトのドメイン
+const baseUrl = 'https://softeni.vercel.app';
+
+function generateSitemap() {
+  let urls = '';
+
+  players.forEach((player) => {
+    urls += `
+  <url>
+    <loc>${baseUrl}/players/${player}/results</loc>
+  </url>
+  <url>
+    <loc>${baseUrl}/players/${player}/information</loc>
+  </url>
+`;
+  });
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+>
+${urls}
+</urlset>
+`;
+
+  fs.writeFileSync(path.join(__dirname, '../public/sitemap.xml'), sitemap.trim());
+  console.log('✅ sitemap.xml を生成しました');
+}
+
+generateSitemap();
