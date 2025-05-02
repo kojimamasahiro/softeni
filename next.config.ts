febfd85f4d1028773700361435c9bd10ac8edace
mkojima <kojima.masahiro.570@gmail.com>
2025-05-03 00:23:24 +0900

    Add environment variables for microcms configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,3 +6,10 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+  env: {
+    MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN,
+    MICROCMS_API_KEY: process.env.MICROCMS_API_KEY,
+  },
+};
