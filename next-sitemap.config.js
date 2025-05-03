3871d86a5548bf6c62839bda67aa1322e242898d
mkojima <kojima.masahiro.570@gmail.com>
2025-05-03 21:42:25 +0900

    Add next-sitemap configuration file and update package.json for sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://softeni.vercel.app',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: 'daily',
+    priority: 0.7,
+  };
+  
