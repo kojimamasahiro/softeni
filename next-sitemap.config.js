ba31c44b4233a98fb39a87088eecaf1eb3058e1d
mkojima <kojima.masahiro.570@gmail.com>
2025-05-11 16:26:53 +0900

    feat: update sitemap configuration and remove lastmod tags for cleaner output

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://softeni.vercel.app',
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-    changefreq: 'daily',
-    priority: 0.7,
-  };
-  
+  siteUrl: 'https://softeni.vercel.app', // サイトのURLを指定
+  generateRobotsTxt: true, // robots.txtを生成
+  changefreq: 'daily', // ページ更新頻度
+  priority: 0.7, // ページ優先度
+  transform: async (config, path) => {
+    return {
+      loc: path, // ページURL
+      // lastmodを省略（削除）することで表示されないようにする
+      // lastmod: undefined,  // もしくは、lastmod を削除
+      changefreq: 'daily', // 頻度
+      priority: 0.7, // 優先度
+    };
+  },
+};
+
+
