6b9a55a3795a55f8e5b6ba5db4d2163b498fcf54
mkojima <kojima.masahiro.570@gmail.com>
2025-05-17 19:44:01 +0900

    fix: uncomment player names in LIVE_PLAYERS array

--- config/livePeriod.ts
+++ config/livePeriod.ts
@@ -7,7 +7,7 @@ export const LIVE_PERIOD = {
 
 export const LIVE_PLAYERS = [
   "uematsu-toshiki",
-  // "ueoka-shunsuke",
-  // "hashiba-shuichiro",
+  "ueoka-shunsuke",
+  "hashiba-shuichiro",
   "uchida-riku",
 ]
