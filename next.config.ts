70e8c4d78ab92376036580cdd391b21f66cb0083
mkojima <kojima.masahiro.570@gmail.com>
2025-05-31 12:14:59 +0900

    refactor: Update Next.js configuration for improved type handling and redirect logic

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,25 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-};
-
-export default nextConfig;
-
-module.exports = {
   env: {
     MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN,
     MICROCMS_API_KEY: process.env.MICROCMS_API_KEY,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'softeni.vercel.app',
+          },
+        ],
+        destination: 'https://softeni-pick.com',
+        permanent: true,
+      },
+    ];
+  },
 };
+
+module.exports = nextConfig;
