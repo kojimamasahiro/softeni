afe3e39a3e1f5afd0d447a4a4f5cfa3705b2b5b8
mkojima <kojima.masahiro.570@gmail.com>
2025-06-18 21:46:45 +0900

    fix: adjust player name position in Open Graph image

--- src/pages/api/og/player.tsx
+++ src/pages/api/og/player.tsx
@@ -36,7 +36,7 @@ export default function handler(req: NextRequest) {
         <div
           style={{
             position: 'absolute',
-            top: '50%',
+            top: '40%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
             fontSize: 128,
