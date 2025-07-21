8cd5fdf5830a67261d08274d4cfbd450be91ec1e
mkojima <kojima.masahiro.570@gmail.com>
2025-07-21 21:13:57 +0900

    feat: update default pairs and scores in tournament overlay for improved clarity

--- src/pages/api/og/tournament.tsx
+++ src/pages/api/og/tournament.tsx
@@ -178,40 +178,40 @@ export default function handler(req: NextRequest) {
   const leftPairs = parseJsonParam<{ name: string; team: string }[]>(
     'leftPairs',
     [
-      { name: '', team: '' },
-      { name: '', team: '' },
-      { name: '', team: '' },
-      { name: '', team: '' },
+      { name: '幡谷康平・端山羅行', team: 'ＮＴＴ東日本東京・稲門クラブ' },
+      { name: '浅見竣一朗・安達宣', team: '早稲田大学' },
+      { name: '齋藤翔一・桑山信', team: '日本信号' },
+      { name: '大村圭志朗・佐藤大和', team: 'アキム' },
     ],
   );
 
   const rightPairs = parseJsonParam<{ name: string; team: string }[]>(
     'rightPairs',
     [
-      { name: '', team: '' },
-      { name: '', team: '' },
-      { name: '', team: '' },
-      { name: '', team: '' },
+      { name: '片岡暁紀・黒坂卓矢', team: '日本体育大学' },
+      { name: '高橋拓己・広岡大河', team: '法政大学' },
+      { name: '品川貴紀・早川和宏', team: '福井県庁' },
+      { name: '田中康文・金子大祐', team: '厚木市役所' },
     ],
   );
 
   const topScoreValues = parseJsonParam<string[]>('topScores', [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
+    '4',
+    '2',
+    '2',
+    '4',
+    '4',
+    '3',
+    '4',
   ]);
   const bottomScoreValues = parseJsonParam<string[]>('bottomScores', [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
+    '2',
+    '4',
+    '4',
+    '1',
+    '0',
+    '4',
+    '1',
   ]);
 
   const overlayPaths = getOverlayPaths(topScoreValues, bottomScoreValues);
