aa5cd74f6acfd2eaa3f64a9db85d0ca3c88ecfe1
mkojima <kojima.masahiro.570@gmail.com>
2025-12-21 22:26:41 +0900

    feat: KantanLinkコンポーネントを削除し、アフィリエイトスクリプトを直接ページに追加

--- src/pages/tournaments/[generation]/[tournamentId]/[year]/[gameCategory]/[ageCategory]/[gender]/index.tsx
+++ src/pages/tournaments/[generation]/[tournamentId]/[year]/[gameCategory]/[ageCategory]/[gender]/index.tsx
@@ -8,7 +8,6 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { useState } from 'react';
 
-import { KantanLink } from '@/components/Ads/KantanLink';
 import Breadcrumbs from '@/components/Breadcrumb';
 import MetaHead from '@/components/MetaHead';
 import MatchResults from '@/components/Tournament/MatchResults';
@@ -228,8 +227,6 @@ export default function TournamentYearResultPage({
             </section>
           )}
 
-          <KantanLink />
-
           <div className="text-right mt-10 mb-2">
             <Link
               href="/tournaments"
@@ -237,6 +234,16 @@ export default function TournamentYearResultPage({
             >
               大会結果一覧
             </Link>
+          </div>
+
+          {/* もしもアフィリエイト 簡単リンク */}
+          <div className="mb-3">
+            <script
+              type="text/javascript"
+              dangerouslySetInnerHTML={{
+                __html: `(function(b,c,f,g,a,d,e){b.MoshimoAffiliateObject=a;\n b[a]=b[a]||function(){arguments.currentScript=c.currentScript\n||c.scripts[c.scripts.length-2];(b[a].q=b[a].q||[]).push(arguments)};\n c.getElementById(a)||(d=c.createElement(f),d.src=g,\n d.id=a,e=c.getElementsByTagName("body")[0],e.appendChild(d))})(window,document,"script","//dn.msmstatic.com/site/cardlink/bundle.js?20220329","msmaflink");\n msmaflink({"n":"【12\\/26 2時まで ポイント最大15倍！】 ルーセント LUCENT テニス ソフトテニスボール アカエム 試合球 ホワイト 1ダース 12球 軟式テニス 軟式球 中学 高校 テニボ 部活 練習 軟式テニス用品 箱販売 ソフトテニス M30000","b":"","t":"","d":"https:\\/\\/thumbnail.image.rakuten.co.jp","c_p":"","p":["\\/@@0_mall\\/sportsaomori\\/cabinet\\/images607\\/sg-m30000_1.jpg"],"u":{"u":"https:\\/\\/item.rakuten.co.jp\\/sportsaomori\\/sg-m30000\\/","t":"rakuten","r_v":""},"v":"2.1","b_l":[{"id":1,"u_tx":"楽天市場で見る","u_bc":"#f76956","u_url":"https:\\/\\/item.rakuten.co.jp\\/sportsaomori\\/sg-m30000\\/","a_id":5314178,"p_id":54,"pl_id":27059,"pc_id":54,"s_n":"rakuten","u_so":0}],"eid":"AuyEZ","s":"l"});`,
+              }}
+            />
           </div>
 
           {detailData && (
