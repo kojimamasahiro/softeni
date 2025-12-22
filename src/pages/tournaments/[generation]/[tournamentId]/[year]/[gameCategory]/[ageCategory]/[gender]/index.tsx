846463ac96f22371e421461d5d27df7ebb0796b4
mkojima <kojima.masahiro.570@gmail.com>
2025-12-22 13:15:08 +0900

    feat: Moshimoアフィリエイトリンクの表示要素にマージンを追加

--- src/pages/tournaments/[generation]/[tournamentId]/[year]/[gameCategory]/[ageCategory]/[gender]/index.tsx
+++ src/pages/tournaments/[generation]/[tournamentId]/[year]/[gameCategory]/[ageCategory]/[gender]/index.tsx
@@ -248,7 +248,9 @@ export default function TournamentYearResultPage({
               __html: `(function(b,c,f,g,a,d,e){b.MoshimoAffiliateObject=a;\n b[a]=b[a]||function(){arguments.currentScript=c.currentScript\n||c.scripts[c.scripts.length-2];(b[a].q=b[a].q||[]).push(arguments)};\n c.getElementById(a)||(d=c.createElement(f),d.src=g,\n d.id=a,e=c.getElementsByTagName("body")[0],e.appendChild(d))})(window,document,"script","//dn.msmstatic.com/site/cardlink/bundle.js?20220329","msmaflink");\n msmaflink({"n":"【12\\/26 2時まで ポイント最大15倍！】 ルーセント LUCENT テニス ソフトテニスボール アカエム 試合球 ホワイト 1ダース 12球 軟式テニス 軟式球 中学 高校 テニボ 部活 練習 軟式テニス用品 箱販売 ソフトテニス M30000","b":"","t":"","d":"https:\\/\\/thumbnail.image.rakuten.co.jp","c_p":"","p":["\\/@0_mall\\/sportsaomori\\/cabinet\\/images607\\/sg-m30000_1.jpg"],"u":{"u":"https:\\/\\/item.rakuten.co.jp\\/sportsaomori\\/sg-m30000\\/","t":"rakuten","r_v":""},"v":"2.1","b_l":[{"id":1,"u_tx":"楽天市場で見る","u_bc":"#f76956","u_url":"https:\\/\\/item.rakuten.co.jp\\/sportsaomori\\/sg-m30000\\/","a_id":5314178,"p_id":54,"pl_id":27059,"pc_id":54,"s_n":"rakuten","u_so":1}],"eid":"wauw8","s":"l"});`,
             }}
           />
-          <div id="msmaflink-wauw8">リンク</div>
+          <div id="msmaflink-wauw8" className="mb-4">
+            リンク
+          </div>
           {/* MoshimoAffiliateEasyLink END */}
 
           {detailData && (
