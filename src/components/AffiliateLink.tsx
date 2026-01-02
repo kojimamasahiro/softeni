3c7fe227f3d106a4ef109da39ee0fa53d3c512c3
mkojima <kojima.masahiro.570@gmail.com>
2026-01-02 23:53:56 +0900

    fix: アフィリエイトリンクの画像サイズとIDを更新

--- src/components/AffiliateLink.tsx
+++ src/components/AffiliateLink.tsx
@@ -18,7 +18,7 @@ export default function AffiliateLink() {
       <div className="flex justify-center mb-4 mt-6">
         <div
           dangerouslySetInnerHTML={{
-            __html: `<a href="//af.moshimo.com/af/c/click?a_id=5325564&p_id=55&pc_id=55&pl_id=631" rel="nofollow" referrerpolicy="no-referrer-when-downgrade" attributionsrc><img src="//image.moshimo.com/af-img/0032/000000000631.gif" width="468" height="60" style="border:none;"></a><img src="//i.moshimo.com/af/i/impression?a_id=5325564&p_id=55&pc_id=55&pl_id=631" width="1" height="1" style="border:none;" loading="lazy">`,
+            __html: `<a href="//af.moshimo.com/af/c/click?a_id=5325564&p_id=55&pc_id=55&pl_id=629" rel="nofollow" referrerpolicy="no-referrer-when-downgrade" attributionsrc><img src="//image.moshimo.com/af-img/0032/000000000629.gif" width="300" height="250" style="border:none;"></a><img src="//i.moshimo.com/af/i/impression?a_id=5325564&p_id=55&pc_id=55&pl_id=629" width="1" height="1" style="border:none;" loading="lazy"></img>`,
           }}
         />
       </div>
